223492b56317e5c05fca13fe8e6fb4d9f84ad45b
Ivan Caseca <icaseca93@gmail.com>
2021-06-24 18:56:51 -0300

    Correção do Erro de divisão por zero

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -13,24 +13,27 @@ print('1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
 opcao = int(input('\nDigite sua opção (1/2/3/4): '))
 
 # Numeros de entrada que o usuario vai dar.
-num1 = int(input('\nDigite um número: '))
-num2 = int(input('\nDigite outro número: '))
+try:
+    num1 = int(input('\nDigite um número: '))
+    num2 = int(input('\nDigite outro número: '))
 
-# confere se a variavel opcao é igual a 1 para ser executada.
-if opcao == 1:
-    print(f'\nA soma de {num1} + {num2} = {num1 + num2}. ')
+    # confere se a variavel opcao é igual a 1 para ser executada.
+    if opcao == 1:
+        print(f'\nA soma de {num1} + {num2} = {num1 + num2}. ')
 
-# caso a primeiro if não for verdade executa esse
-elif opcao == 2:
-    print(f'\nA subtração de {num1} + {num2} = {num1 - num2}.')
+    # caso a primeiro if não for verdade executa esse
+    elif opcao == 2:
+        print(f'\nA subtração de {num1} + {num2} = {num1 - num2}.')
 
-# caso os dois não forem verdade, executa esse.
-elif opcao == 3:
-    print(f'\nA multiplicação de {num1} + {num2} = {num1 * num2}.')
+    # caso os dois não forem verdade, executa esse.
+    elif opcao == 3:
+        print(f'\nA multiplicação de {num1} + {num2} = {num1 * num2}.')
 
-# caso os tres não forem verdade, executa esse.
-elif opcao == 4:
-    print(f'\nA divisão de {num1} + {num2} = {num1 / num2}.')
-# Se nenhum for verdade, executa esse.
-else:
-    print('\nOpção Inválida!')
+    # caso os tres não forem verdade, executa esse.
+    elif opcao == 4:
+        print(f'\nA divisão de {num1} + {num2} = {num1 / num2}.')
+    # Se nenhum for verdade, executa esse.
+    else:
+        print('\nOpção Inválida!')
+except ZeroDivisionError:
+    print('Não é possível executar uma divisão por zero.')
